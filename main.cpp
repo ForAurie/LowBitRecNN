497ed226ea7461ad6406e5b76b24cd85a36c5336
ForAurie <Aurora-A-0@outlook.com>
2025-12-25 12:01:08 +0800

    updata main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <bits/stdc++.h>
+#include "LinearAlgebra.hpp"
+#include <random>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstdint>
+#include <string>
+#include <fstream>
 // 全局常量
 using nT = std::uint16_t;
-constexpr nT N = 1 << 11;
+constexpr int N = 1 << 11;
 // ----------
 
 // 类的定义
@@ -60,8 +72,8 @@ BT sigmoid(BT x) {
     return BT(__SIGMOID[x.get()]);
 }
 BT f(BT x, BT y) { return BT(__F[x.get()][y.get()]); }
-double sigmoid(double x) { return 1.0 / (1.0 + std::exp(-x)); }
-double f(double a, double x) { return  a / (a + (1 - a) * exp(-x)); }
+double sigmoidf(double x) { return 1.0 / (1.0 + std::exp(-x)); }
+double ff(double a, double x) { return  a / (a + (1 - a) * exp(-x)); }
 // ----------
 
 void init() { // 预处理
@@ -86,115 +98,144 @@ void init() { // 预处理
             __DIV[i][j] = BT(BT(i).val() / BT(j).val()).get();
 }
 
-#include "LinearAlgebra.hpp"
-// #include <random>
-// #include <iostream>
-// #include <utility>
-// #include <vector>
-// #include <string>
-// #include <cstdio>
-// #include <cassert>
-// #include <cmath>
-// #include <ctime>
+// Net
 
-// 改版 QNet，只有前向传播
-template<typename T = float, typename Mat = LinearAlgebra::Matrix<T>, const bool haveBiases = true, const bool haveOutputActivation = true>
-class QNet {
-    private:
-        size_t inputSize, outputSize;
-        std::vector<Mat> weights, biases;
-        std::vector<std::pair<Mat, Mat>> __forward(Mat input) const {
-            std::vector<std::pair<Mat, Mat>> res;
-            for (size_t i = 0; i < weights.size() - 1; i++) {
-                input *= weights[i];
-                if (haveBiases) input += biases[i];
-                res.push_back(std::make_pair(input, Mat()));
-                input.applyFunctionSelf(actFunc);
-                res.back().second = input;
-            }
-            input *= weights.back();
-            if (haveBiases) input += biases.back();
-            res.push_back(std::make_pair(input, Mat()));
-            if (haveOutputActivation) {
-                input.applyFunctionSelf(actFunc);
-                res.back().second = input;
-            }
-            return res;
-        }
-    public:
-        const size_t& getInputSize() const { return inputSize; }
-        const size_t& getOutputSize() const { return outputSize; }
-        std::vector<Mat>& getWeights() { return weights; }
-        std::vector<Mat>& getBiases() { return biases; }
-        const std::vector<Mat>& getWeights() const { return weights; }
-        const std::vector<Mat>& getBiases() const { return biases; }
-        ANN(const size_t& __inputSize, const size_t& __outputSize, const std::vector<size_t>& hiddenLayers): inputSize(__inputSize), outputSize(__outputSize), weights(hiddenLayers.size() + 1), biases(0) {
-            if (haveBiases) biases.resize(hiddenLayers.size() + 1);
-            weights.front().resize(inputSize, hiddenLayers.front());
-            if (haveBiases) biases.front().resize(1, hiddenLayers.front());
-            weights.back().resize(hiddenLayers.back(), outputSize);
-            if (haveBiases) biases.back().resize(1, outputSize);
-            for (size_t i = 1; i < hiddenLayers.size(); ++i) {
-                weights[i].resize(hiddenLayers[i - 1], hiddenLayers[i]);
-                if (haveBiases) biases[i].resize(1, hiddenLayers[i]);
-            }
-        }
-        Mat forward(Mat input) const {
-            for (size_t i = 0; i + 1 < weights.size(); ++i) {
-                input *= weights[i];
-                if (haveBiases) input += biases[i];
-                input.applyFunctionSelf(actFunc);
-            }
-            input *= weights.back();
-            if (haveBiases) input += biases.back();
-            if (haveOutputActivation) input.applyFunctionSelf(actFunc);
-            return input;
-        }
-        void open(const std::string& filename = "QANN.model") {
-            FILE *fin = fopen(filename.c_str(), "rb");
-            char sign;
-            unsigned int typeSize;
-            fread(&sign, sizeof(char), 1, fin);
-            assert(sign == 'Q'); // 检查标志
-            fread(&typeSize, sizeof(unsigned int), 1, fin);
-            assert(typeSize == sizeof(T)); // 检查数据类型大小
-            bool __haveBiases, __haveOutputActivation;
-            fread(&__haveBiases, sizeof(bool), 1, fin);
-            fread(&__haveOutputActivation, sizeof(bool), 1, fin);
-            assert(__haveBiases == haveBiases && __haveOutputActivation == haveOutputActivation); // 检查偏置和输出层激活函数一致性
-            fread(&inputSize, sizeof(size_t), 1, fin);
-            fread(&outputSize, sizeof(size_t), 1, fin);
-            size_t layers;
-            fread(&layers, sizeof(size_t), 1, fin);
-            weights.resize(layers);
-            if (haveBiases) biases.resize(layers);
-            T tmp;
-            for (auto &x: weights) {
-                size_t n, m;
-                fread(&n, sizeof(size_t), 1, fin);
-                fread(&m, sizeof(size_t), 1, fin);
-                x.resize(n, m);
-                for (size_t i = 0; i < n; i++) {
-                    for (size_t j = 0; j < m; j++) {
-                        fread(&tmp, sizeof(T), 1, fin);
-                        x(i, j) = tmp;
-                    }
+
+namespace QNet {
+    template<typename T = float, typename Mat = LinearAlgebra::Matrix<T>, auto actFunc = sigmoidf, auto f = ff>
+    class Net {
+        private:
+            std::vector<Mat> weights, biases;
+        public:
+            size_t getInputSize() const { return weights.front().N(); }
+            size_t getOutputSize() const { return weights.back().M(); }
+            std::vector<Mat>& getWeights() { return weights; }
+            std::vector<Mat>& getBiases() { return biases; }
+            const std::vector<Mat>& getWeights() const { return weights; }
+            const std::vector<Mat>& getBiases() const { return biases; }
+            Net(size_t inputSize, const std::vector<size_t>& Layers): weights(Layers.size()), biases(0) {
+                if (haveBiases) biases.resize(Layers.size());
+                weights.front().resize(inputSize, Layers.front());
+                if (haveBiases) biases.front().resize(1, Layers.front());
+                for (size_t i = 1; i < Layers.size(); ++i) {
+                    weights[i].resize(Layers[i - 1], Layers[i]);
+                    if (haveBiases) biases[i].resize(1, Layers[i]);
                 }
             }
-            if (haveBiases) {
-                for (auto &x: biases) {
-                    size_t n;
+            Mat forward(Mat input) const {
+                for (size_t i = 0; i < weights.size(); ++i) {
+                    Mat next(1, weights[i].M(), actFunc(0));
+                    for (size_t j = 0; j < weights[i].N(); j++) {
+                        for (size_t k = 0; k < weights[i].M(); k++) {
+                            next(0, k) += f(next(0, k), input(0, j) * weights[i](j, k));
+                        }
+                    }
+                    input = next;
+                    if (haveBiases) {
+                        for (size_t j = 0; j < input.M(); j++) {
+                            input(0, j) += f(input(0, j), biases[i](0, j));
+                        }
+                    }
+                }
+                return input;
+            }
+            void open(const std::string& filename) {
+                FILE *fin = fopen(filename.c_str(), "rb");
+                char sign;
+                unsigned int typeSize;
+                fread(&sign, sizeof(char), 1, fin);
+                assert(sign == 'Q'); // 检查标志
+                fread(&typeSize, sizeof(unsigned int), 1, fin);
+                assert(typeSize == sizeof(T)); // 检查数据类型大小
+                bool __haveBiases, __haveOutputActivation;
+                fread(&__haveBiases, sizeof(bool), 1, fin);
+                fread(&__haveOutputActivation, sizeof(bool), 1, fin);
+                assert(__haveBiases == haveBiases && __haveOutputActivation == haveOutputActivation); // 检查偏置和输出层激活函数一致性
+                size_t layers;
+                fread(&layers, sizeof(size_t), 1, fin);
+                weights.resize(layers);
+                if (haveBiases) biases.resize(layers);
+                T tmp;
+                for (auto &x: weights) {
+                    size_t n, m;
                     fread(&n, sizeof(size_t), 1, fin);
-                    x.resize(1, n);
+                    fread(&m, sizeof(size_t), 1, fin);
+                    x.resize(n, m);
                     for (size_t i = 0; i < n; i++) {
-                        fread(&tmp, sizeof(T), 1, fin);
-                        x(0, i) = tmp;
+                        for (size_t j = 0; j < m; j++) {
+                            fread(&tmp, sizeof(T), 1, fin);
+                            x(i, j) = tmp;
+                        }
                     }
                 }
+                if (haveBiases) {
+                    for (auto &x: biases) {
+                        size_t n;
+                        fread(&n, sizeof(size_t), 1, fin);
+                        x.resize(1, n);
+                        for (size_t i = 0; i < n; i++) {
+                            fread(&tmp, sizeof(T), 1, fin);
+                            x(0, i) = tmp;
+                        }
+                    }
+                }
+                fclose(fin);
             }
-            fclose(fin);
+    };
+}
+
+// ----------
+
+using namespace std;
+using Mat = LinearAlgebra::Matrix<float>;
+#define os cout
+void loadData(const string& Path, vector<Mat>& inputs, vector<Mat>& targets) {
+    ifstream fin(Path);
+    int tmp;
+    for (size_t i = 0; i < 10; i++) {
+        size_t sz; Mat ans(1, 10, 0), in(1, 28 * 28, 0); ans(0, i) = 1;
+        fin >> i >> sz;
+        os << "Loading " << sz << " samples for digit " << i << endl;
+        while (sz--) {
+            for (size_t j = 0; j < 28 * 28; j++)
+                fin >> tmp, in(0, j) = tmp;
+            inputs.push_back(in);
+            targets.push_back(ans);
         }
-};
+    }
+    fin.close();
+}
+int predict(const Mat& output) {
+    int maxIndex = 0;
+    for (size_t i = 1; i < output.M(); i++)
+        if (output(0, i) > output(0, maxIndex))
+            maxIndex = i;
+    return maxIndex;
+}
+
+int main() {
+    os << "Loading ANN model..." << endl;
+    QNet::Net<float, Mat, sigmoidf, ff> nn(784, {256, 64, 10});
+    cout << "Path: ";
+	string Path;
+	cin >> Path;
+	cout << endl;
+	nn.open(Path);
+
+    os << "Loading Testing data..." << endl;
+    vector<Mat> inputs, targets;
+    loadData(".\\TestData.txt", inputs, targets);
+
+    os << "Testing..." << endl;
+    int correct = 0;
+    for (size_t i = 0; i < inputs.size(); i++) {
+        correct += (predict(nn.forward(inputs[i])) == predict(targets[i]));
+    }
+    os << "Testing completed." << endl;
+    os << "Accuracy: " << correct << " / " << inputs.size() << " = " << (correct * 100.0 / inputs.size()) << "%" << endl;
+    int tmp; while (cin >> tmp);
+    return 0;
+}
 
 int main() {
     init();
